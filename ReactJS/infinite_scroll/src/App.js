bde94154dadf38132c9dbc73faa915350fe6281d
Prashant <prashant.jha@zaggle.in>
2022-12-30 22:01:11 +0530

    Started working on Counter app

--- ReactJS/infinite_scroll/src/App.js
+++ ReactJS/infinite_scroll/src/App.js
@@ -1,38 +1,23 @@
-import {useState, useEffect, useRef, useCallback} from 'react';
-import fetchData from './FetchData';
+import {useEffect, useState} from 'react';
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [isLoading, setIsLoading] = useState(false)
-  const [searchResults, setSearchresults] = useState([]);
-  const pageNumber = useRef(1)
-  const observer = useRef(null);
-  const lastElementObserver = useCallback(node=>{
-    if(isLoading) return
-    if(observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver(entries=>{
-      if(entries[0].isIntersecting){
-        pageNumber.current+=1
-        setIsLoading(true)
-        fetchData(searchTerm, pageNumber, setIsLoading, setSearchresults)
-      }
-    })
-    if(node) observer.current.observe(node)
-  })
+  const [posts, setPosts] = useState([]);
 
-  useEffect(()=>{
-    fetchData(searchTerm, pageNumber, setIsLoading, setSearchresults)
-  }, [searchTerm])
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/posts').then(value=>value.json()).then(value=>{
+    setPosts(value)
+  })
+})
+
+
   return(
     <div>
-    <input onChange={(event)=>{
-      setIsLoading(true)
-      setSearchTerm(event.target.value)
-    }}></input>
-    {searchResults.length ? searchResults.map(({title}, index)=>
-      <div ref={index===searchResults.length-1?lastElementObserver:null}>{title}</div>
-    ):''}
-    <p>{isLoading?'loading...':''}</p>
+      <input onChange={(event)=>{
+        setPosts(posts.filter((value)=>value.title.includes(event.target.value)))
+      }}></input>
+    {posts.length && posts.map(({title})=><p>{title}</p>)}
     </div>
+
   )
+
 }
 export default App;
